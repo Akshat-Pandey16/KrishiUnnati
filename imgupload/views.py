b039f10201de993357d17a5e3f2827a70ddb0ffe
Akshat Pandey <aplinkinpark16@gmail.com>
2024-03-24 11:59:20 +0530

    views update

--- imgupload/views.py
+++ imgupload/views.py
@@ -3,18 +3,24 @@ from .forms import ImageUploadForm
 from .test01 import identify_plant
 from django.contrib import messages
 
-# Create your views here.
 def imgupload(request):
     return render(request,'plant.html')
 
 def result(request):
     return render(request,'index.html')
 
+def recursive_items(dictionary):
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            yield from recursive_items(value)
+        else:
+            yield key, value
+
+
 def imageprocess(request): 
     form=ImageUploadForm(request.POST, request.FILES )
     if form.is_valid():
         handel_uploaded_file(request.FILES['image'])
-        img_path = 'img.jpg'
         x=identify_plant(["img.jpg"])
         res_orginal=[]
         res_value=[]
@@ -101,16 +107,15 @@ def imageprocess(request):
             return redirect(imgupload)
             
 
-        t=tables()
-        for x in range(3):
-            con=sql_connection()
-            mycursor = con.cursor()
-            querry="insert into plant values(%s,%s,%s,%s,now())"
+        # for x in range(3):
+        #     con=sql_connection()
+        #     mycursor = con.cursor()
+        #     querry="insert into plant values(%s,%s,%s,%s,now())"
             
-            data=[str(plant_name),str(name[x]),probability[x],'bengluru']
-            mycursor.execute(querry,data)
-            mycursor.close()
-            con.commit()
+        #     data=[str(plant_name),str(name[x]),probability[x],'bengluru']
+        #     mycursor.execute(querry,data)
+        #     mycursor.close()
+        #     con.commit()
 
         return render(request,'result.html' , {'res_orginal':res_orginal,'res_value':res_value[0],'img_url':res_value[1],'pclass':pclass,'family':family,'genus':genus,'kindom':kindom,'order':order,'phylum':phylum,'org_url':org_url,'plant_name':plant_name,
         
@@ -121,20 +126,20 @@ def handel_uploaded_file(f):
         for chunk in f.chunks():
             destination.write(chunk)
 
-def report(request):
-    con=sql_connection()
-    mycursor = con.cursor()
-    querry="select d1,count(d1),round(avg(percentage)*100,2) from plant where uploaddate BETWEEN (NOW() - INTERVAL 7 DAY) AND NOW() group by d1 order by 2 desc;"
-    mycursor.execute(querry)
-    myresult=mycursor.fetchall()
-    name=[]
-    frequency=[]
-    percentage=[]
+# def report(request):
+#     con=sql_connection()
+#     mycursor = con.cursor()
+#     querry="select d1,count(d1),round(avg(percentage)*100,2) from plant where uploaddate BETWEEN (NOW() - INTERVAL 7 DAY) AND NOW() group by d1 order by 2 desc;"
+#     mycursor.execute(querry)
+#     myresult=mycursor.fetchall()
+#     name=[]
+#     frequency=[]
+#     percentage=[]
     
-    for x in myresult:
-        # print(x[0],x[1])
-        name.append(x[0])
-        frequency.append(x[1])
-        percentage.append(x[2])
-    zipped=zip(name,frequency,percentage)
-    return render(request,"report.html",{'zipped':zipped})
+#     for x in myresult:
+#         # print(x[0],x[1])
+#         name.append(x[0])
+#         frequency.append(x[1])
+#         percentage.append(x[2])
+#     zipped=zip(name,frequency,percentage)
+#     return render(request,"report.html",{'zipped':zipped})
